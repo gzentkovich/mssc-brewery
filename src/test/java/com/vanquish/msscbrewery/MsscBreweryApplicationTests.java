8af47c2c3fa36a88e7499781f669a61d39a1062d
Gregory Zentkovich <gzentkovich@gmail.com>
2020-11-18 10:01:53 -1000

    add starter spring setup and dependencies

---
+++ src/test/java/com/vanquish/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanquish.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
